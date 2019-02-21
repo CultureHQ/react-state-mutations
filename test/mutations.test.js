fdb04d9632a00d173d2fbbea523345f1a028eddf
Kevin Deisz <kevin.deisz@gmail.com>
2019-02-21 18:03:59 -0500

    Support for react hooks

--- test/mutations.test.js
+++ test/mutations.test.js
@@ -1,249 +1,464 @@
+import { act, cleanup, testHook } from "react-testing-library";
+
 import {
   makeStandaloneMutation,
   makeArgumentMutation,
-  append,
-  concat,
-  cycle,
-  decrement,
+  makeStandaloneHook,
+  makeArgumentHook,
+  append, useAppendState,
+  concat, useConcatState,
+  cycle, useCycleState,
+  decrement, useDecrementState,
   direct,
-  filter,
-  increment,
-  map,
+  filter, useFilterState,
+  increment, useIncrementState,
+  map, useMapState,
   mutate,
-  prepend,
-  toggle,
+  prepend, usePrependState,
+  toggle, useToggleState,
   combineMutations
 } from "../src/mutations";
 
-test("makeStandaloneMutation", () => {
-  const mutation = makeStandaloneMutation(value => value + 50)("object");
-  const prevState = { object: 1 };
-  const nextState = mutation(prevState);
+afterEach(cleanup);
 
-  expect(nextState.object).toEqual(51);
-  expect(prevState.object).toEqual(1);
-});
+describe("factories", () => {
+  test("makeStandaloneMutation", () => {
+    const mutation = makeStandaloneMutation(value => value + 50)("object");
+    const prevState = { object: 1 };
+    const nextState = mutation(prevState);
 
-test("makeArgumentMutation", () => {
-  const mutation = makeArgumentMutation(object => value => value + object)("object")(50);
-  const prevState = { object: 1 };
-  const nextState = mutation(prevState);
+    expect(nextState.object).toEqual(51);
+    expect(prevState.object).toEqual(1);
+  });
 
-  expect(nextState.object).toEqual(51);
-  expect(prevState.object).toEqual(1);
-});
+  test("makeArgumentMutation", () => {
+    const mutation = makeArgumentMutation(object => value => value + object)("object")(50);
+    const prevState = { object: 1 };
+    const nextState = mutation(prevState);
 
-test("append", () => {
-  const mutation = append("objects")(4);
-  const prevState = { objects: [1, 2, 3] };
-  const nextState = mutation(prevState);
+    expect(nextState.object).toEqual(51);
+    expect(prevState.object).toEqual(1);
+  });
 
-  expect(nextState.objects[nextState.objects.length - 1]).toEqual(4);
-  expect(prevState.objects.length).toEqual(3);
-});
+  test("makeStandaloneHook", () => {
+    const useDoubleState = makeStandaloneHook(value => value * 2);
 
-test("concat", () => {
-  const mutation = concat("objects")([4, 5, 6]);
-  const prevState = { objects: [1, 2, 3] };
-  const nextState = mutation(prevState);
+    let count, onDouble;
+    testHook(() => ([count, onDouble] = useDoubleState(1)));
 
-  expect(nextState.objects.length).toEqual(6);
-  expect(nextState.objects[nextState.objects.length - 1]).toEqual(6);
-  expect(prevState.objects.length).toEqual(3);
-});
+    expect(count).toBe(1);
 
-test("cycle", () => {
-  const mutation = cycle("object")(["foo", "bar", "baz"]);
-  const prevState = { object: "foo" };
+    act(() => onDouble());
+    expect(count).toBe(2);
 
-  let nextState = mutation(prevState);
-  expect(nextState.object).toEqual("bar");
-  expect(prevState.object).toEqual("foo");
+    act(() => onDouble());
+    expect(count).toBe(4);
+  });
 
-  nextState = mutation(nextState);
-  expect(nextState.object).toEqual("baz");
+  test("makeArgumentHook", () => {
+    const useAddHook = makeArgumentHook(object => value => value + object);
 
-  nextState = mutation(nextState);
-  expect(nextState.object).toEqual("foo");
-});
+    let count, onAdd;
+    testHook(() => ([count, onAdd] = useAddHook(0)));
 
-test("decrement", () => {
-  const mutation = decrement("object");
-  const prevState = { object: 1 };
-  const nextState = mutation(prevState);
+    expect(count).toBe(0);
 
-  expect(nextState.object).toEqual(0);
-  expect(prevState.object).toEqual(1);
-});
+    act(() => onAdd(10));
+    expect(count).toBe(10);
 
-test("direct", () => {
-  const mutation = direct("object")("bar");
-  const prevState = { object: "foo" };
-  const nextState = mutation(prevState);
-
-  expect(nextState.object).toEqual("bar");
-  expect(prevState.object).toEqual("foo");
-});
-
-test("filter", () => {
-  const mutation = filter("objects")(value => value % 2 === 0);
-  const prevState = { objects: [1, 2, 3, 4, 5, 6] };
-  const nextState = mutation(prevState);
-
-  expect(nextState.objects).toEqual([2, 4, 6]);
-  expect(prevState.objects).toEqual([1, 2, 3, 4, 5, 6]);
-});
-
-test("increment", () => {
-  const mutation = increment("object");
-  const prevState = { object: 1 };
-  const nextState = mutation(prevState);
-
-  expect(nextState.object).toEqual(2);
-  expect(prevState.object).toEqual(1);
-});
-
-test("map", () => {
-  const mutation = map("objects")(value => value * 2);
-  const prevState = { objects: [1, 2, 3] };
-  const nextState = mutation(prevState);
-
-  expect(nextState.objects).toEqual([2, 4, 6]);
-  expect(prevState.objects).toEqual([1, 2, 3]);
-});
-
-test("mutate", () => {
-  const mutation = mutate("object")({ a: "c" });
-  const prevState = { object: { a: "b" } };
-  const nextState = mutation(prevState);
-
-  expect(nextState.object.a).toEqual("c");
-  expect(prevState.object.a).toEqual("b");
-});
-
-test("prepend", () => {
-  const mutation = prepend("objects")(0);
-  const prevState = { objects: [1, 2, 3] };
-  const nextState = mutation(prevState);
-
-  expect(nextState.objects[0]).toEqual(0);
-  expect(prevState.objects.length).toEqual(3);
-});
-
-test("toggle", () => {
-  const mutation = toggle("object");
-  const prevState = { object: true };
-  const nextState = mutation(prevState);
-
-  expect(nextState.object).toBe(false);
-  expect(prevState.object).toBe(true);
-});
-
-test("combineMutations", () => {
-  const mutation = combineMutations(
-    append("appendable"),
-    decrement("decrementable"),
-    increment("incrementable"),
-    mutate("mutatable"),
-    prepend("prependable"),
-    toggle("toggleable")
-  );
-
-  const prevState = {
-    appendable: [1, 2, 3],
-    decrementable: 1,
-    incrementable: 1,
-    mutatable: { a: "b" },
-    prependable: [1, 2, 3],
-    toggleable: true
-  };
-
-  expect(mutation(4, { a: "c" }, 0)(prevState)).toEqual({
-    appendable: [1, 2, 3, 4],
-    decrementable: 0,
-    incrementable: 2,
-    mutatable: { a: "c" },
-    prependable: [0, 1, 2, 3],
-    toggleable: false
+    act(() => onAdd(20));
+    expect(count).toBe(30);
   });
 });
 
-test("combineMutations with all standalones", () => {
-  const mutation = combineMutations(
-    decrement("decrementable"),
-    increment("incrementable"),
-    toggle("toggleable")
-  );
+describe("appending", () => {
+  test("append", () => {
+    const mutation = append("objects")(4);
+    const prevState = { objects: [1, 2, 3] };
+    const nextState = mutation(prevState);
 
-  const prevState = {
-    decrementable: 1,
-    incrementable: 1,
-    toggleable: true
-  };
+    expect(nextState.objects[nextState.objects.length - 1]).toEqual(4);
+    expect(prevState.objects.length).toEqual(3);
+  });
 
-  expect(mutation(prevState)).toEqual({
-    decrementable: 0,
-    incrementable: 2,
-    toggleable: false
+  test("useAppendState", () => {
+    let value, onAppend;
+    testHook(() => ([value, onAppend] = useAppendState([1])));
+
+    expect(value).toEqual([1]);
+
+    act(() => onAppend(2));
+    expect(value).toEqual([1, 2]);
+
+    act(() => onAppend(3));
+    expect(value).toEqual([1, 2, 3]);
   });
 });
 
-test("combineMutations with plain objects", () => {
-  const mutation = combineMutations(
-    append("appendable"),
-    { a: "a" },
-    toggle("toggleable"),
-    { b: "b", c: "c" }
-  );
+describe("concat-ing", () => {
+  test("concat", () => {
+    const mutation = concat("objects")([4, 5, 6]);
+    const prevState = { objects: [1, 2, 3] };
+    const nextState = mutation(prevState);
 
-  const prevState = {
-    appendable: [1, 2, 3],
-    a: "b",
-    toggleable: true,
-    b: "c",
-    c: "d"
-  };
+    expect(nextState.objects.length).toEqual(6);
+    expect(nextState.objects[nextState.objects.length - 1]).toEqual(6);
+    expect(prevState.objects.length).toEqual(3);
+  });
 
-  expect(mutation(4)(prevState)).toEqual({
-    appendable: [1, 2, 3, 4],
-    a: "a",
-    toggleable: false,
-    b: "b",
-    c: "c"
+  test("useConcatState", () => {
+    let value, onConcat;
+    testHook(() => ([value, onConcat] = useConcatState([1])));
+
+    expect(value).toEqual([1]);
+
+    act(() => onConcat([2, 3]));
+    expect(value).toEqual([1, 2, 3]);
+
+    act(() => onConcat([4, 5]));
+    expect(value).toEqual([1, 2, 3, 4, 5]);
   });
 });
 
-test("combineMutations with plain objects with all standalones", () => {
-  const mutation = combineMutations(
-    increment("incrementable"),
-    { a: "a" },
-    toggle("toggleable"),
-    { b: "b", c: "c" }
-  );
+describe("prepending", () => {
+  test("prepend", () => {
+    const mutation = prepend("objects")(0);
+    const prevState = { objects: [1, 2, 3] };
+    const nextState = mutation(prevState);
 
-  const prevState = {
-    incrementable: 1,
-    a: "b",
-    toggleable: true,
-    b: "c",
-    c: "d"
-  };
+    expect(nextState.objects[0]).toEqual(0);
+    expect(prevState.objects.length).toEqual(3);
+  });
 
-  expect(mutation(prevState)).toEqual({
-    incrementable: 2,
-    a: "a",
-    toggleable: false,
-    b: "b",
-    c: "c"
+  test("usePrependState", () => {
+    let value, onPrepend;
+    testHook(() => ([value, onPrepend] = usePrependState([3])));
+
+    expect(value).toEqual([3]);
+
+    act(() => onPrepend(2));
+    expect(value).toEqual([2, 3]);
+
+    act(() => onPrepend(1));
+    expect(value).toEqual([1, 2, 3]);
   });
 });
 
-test("combineMutations with arguments works when called multiple times", () => {
-  const mutation = combineMutations(append("appendable"), { a: "b" })(1);
+describe("cycling", () => {
+  test("cycle", () => {
+    const mutation = cycle("object")(["foo", "bar", "baz"]);
+    const prevState = { object: "foo" };
 
-  const prevState = { appendable: [1], a: "a" };
-  const nextState = mutation(prevState);
+    let nextState = mutation(prevState);
+    expect(nextState.object).toEqual("bar");
+    expect(prevState.object).toEqual("foo");
 
-  expect(nextState).toEqual({ appendable: [1, 1], a: "b" });
-  expect(mutation(nextState)).toEqual({ appendable: [1, 1, 1], a: "b" });
+    nextState = mutation(nextState);
+    expect(nextState.object).toEqual("baz");
+
+    nextState = mutation(nextState);
+    expect(nextState.object).toEqual("foo");
+  });
+
+  test("useCycleState", () => {
+    let value, onCycle;
+    testHook(() => ([value, onCycle] = useCycleState([1, 2, 3])));
+
+    expect(value).toEqual(1);
+
+    act(() => onCycle());
+    expect(value).toEqual(2);
+
+    act(() => onCycle());
+    expect(value).toEqual(3);
+
+    act(() => onCycle());
+    expect(value).toEqual(1);
+  });
+});
+
+describe("decrementing", () => {
+  test("decrement", () => {
+    const mutation = decrement("object");
+    const prevState = { object: 1 };
+    const nextState = mutation(prevState);
+
+    expect(nextState.object).toEqual(0);
+    expect(prevState.object).toEqual(1);
+  });
+
+  test("useDecrementState", () => {
+    let count, onDecrement;
+    testHook(() => ([count, onDecrement] = useDecrementState(0)));
+
+    expect(count).toBe(0);
+
+    act(() => onDecrement());
+    expect(count).toBe(-1);
+
+    act(() => onDecrement());
+    expect(count).toBe(-2);
+  });
+
+  test("useDecrementState default", () => {
+    let count, onDecrement;
+    testHook(() => ([count, onDecrement] = useDecrementState()));
+
+    expect(count).toBe(0);
+  });
+});
+
+describe("incrementing", () => {
+  test("increment", () => {
+    const mutation = increment("object");
+    const prevState = { object: 1 };
+    const nextState = mutation(prevState);
+
+    expect(nextState.object).toEqual(2);
+    expect(prevState.object).toEqual(1);
+  });
+
+  test("useIncrementState", () => {
+    let count, onIncrement;
+    testHook(() => ([count, onIncrement] = useIncrementState(0)));
+
+    expect(count).toBe(0);
+
+    act(() => onIncrement());
+    expect(count).toBe(1);
+
+    act(() => onIncrement());
+    expect(count).toBe(2);
+  });
+
+  test("useIncrementState default", () => {
+    let count, onIncrement;
+    testHook(() => ([count, onIncrement] = useIncrementState()));
+
+    expect(count).toBe(0);
+  });
+});
+
+describe("toggling", () => {
+  test("toggle", () => {
+    const mutation = toggle("object");
+    const prevState = { object: true };
+    const nextState = mutation(prevState);
+
+    expect(nextState.object).toBe(false);
+    expect(prevState.object).toBe(true);
+  });
+
+  test("useToggleState", () => {
+    let value, onToggle;
+    testHook(() => ([value, onToggle] = useToggleState(false)));
+
+    expect(value).toBe(false);
+
+    act(() => onToggle());
+    expect(value).toBe(true);
+
+    act(() => onToggle());
+    expect(value).toBe(false);
+  });
+
+  test("useToggleState default", () => {
+    let count, onToggle;
+    testHook(() => ([count, onToggle] = useToggleState()));
+
+    expect(count).toBe(true);
+  });
+});
+
+describe("filtering", () => {
+  test("filter", () => {
+    const mutation = filter("objects")(value => value % 2 === 0);
+    const prevState = { objects: [1, 2, 3, 4, 5, 6] };
+    const nextState = mutation(prevState);
+
+    expect(nextState.objects).toEqual([2, 4, 6]);
+    expect(prevState.objects).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+
+  test("useFilterState", () => {
+    let value, onFilter;
+    testHook(() => ([value, onFilter] = useFilterState([1, 2, 3, 4, 5, 6, 7, 8])));
+
+    expect(value).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+
+    act(() => onFilter((current, index) => index < (value.length / 2)));
+    expect(value).toEqual([1, 2, 3, 4]);
+
+    act(() => onFilter((current, index) => index < (value.length / 2)));
+    expect(value).toEqual([1, 2]);
+  });
+
+  test("useFilterState default", () => {
+    let count, onFilter;
+    testHook(() => ([count, onFilter] = useFilterState()));
+
+    expect(count).toEqual([]);
+  });
+});
+
+describe("mapping", () => {
+  test("map", () => {
+    const mutation = map("objects")(value => value * 2);
+    const prevState = { objects: [1, 2, 3] };
+    const nextState = mutation(prevState);
+
+    expect(nextState.objects).toEqual([2, 4, 6]);
+    expect(prevState.objects).toEqual([1, 2, 3]);
+  });
+
+  test("useMapState", () => {
+    let value, onMap;
+    testHook(() => ([value, onMap] = useMapState([1, 2, 3])));
+
+    expect(value).toEqual([1, 2, 3]);
+
+    act(() => onMap(item => item * 2));
+    expect(value).toEqual([2, 4, 6]);
+
+    act(() => onMap(item => item * 4));
+    expect(value).toEqual([8, 16, 24]);
+  });
+
+  test("useMapState default", () => {
+    let count, onMap;
+    testHook(() => ([count, onMap] = useMapState()));
+
+    expect(count).toEqual([]);
+  });
+});
+
+describe("other mutations", () => {
+  test("direct", () => {
+    const mutation = direct("object")("bar");
+    const prevState = { object: "foo" };
+    const nextState = mutation(prevState);
+
+    expect(nextState.object).toEqual("bar");
+    expect(prevState.object).toEqual("foo");
+  });
+
+  test("mutate", () => {
+    const mutation = mutate("object")({ a: "c" });
+    const prevState = { object: { a: "b" } };
+    const nextState = mutation(prevState);
+
+    expect(nextState.object.a).toEqual("c");
+    expect(prevState.object.a).toEqual("b");
+  });
+});
+
+describe("combineMutations", () => {
+  test("basic", () => {
+    const mutation = combineMutations(
+      append("appendable"),
+      decrement("decrementable"),
+      increment("incrementable"),
+      mutate("mutatable"),
+      prepend("prependable"),
+      toggle("toggleable")
+    );
+
+    const prevState = {
+      appendable: [1, 2, 3],
+      decrementable: 1,
+      incrementable: 1,
+      mutatable: { a: "b" },
+      prependable: [1, 2, 3],
+      toggleable: true
+    };
+
+    expect(mutation(4, { a: "c" }, 0)(prevState)).toEqual({
+      appendable: [1, 2, 3, 4],
+      decrementable: 0,
+      incrementable: 2,
+      mutatable: { a: "c" },
+      prependable: [0, 1, 2, 3],
+      toggleable: false
+    });
+  });
+
+  test("with all standalones", () => {
+    const mutation = combineMutations(
+      decrement("decrementable"),
+      increment("incrementable"),
+      toggle("toggleable")
+    );
+
+    const prevState = {
+      decrementable: 1,
+      incrementable: 1,
+      toggleable: true
+    };
+
+    expect(mutation(prevState)).toEqual({
+      decrementable: 0,
+      incrementable: 2,
+      toggleable: false
+    });
+  });
+
+  test("with plain objects", () => {
+    const mutation = combineMutations(
+      append("appendable"),
+      { a: "a" },
+      toggle("toggleable"),
+      { b: "b", c: "c" }
+    );
+
+    const prevState = {
+      appendable: [1, 2, 3],
+      a: "b",
+      toggleable: true,
+      b: "c",
+      c: "d"
+    };
+
+    expect(mutation(4)(prevState)).toEqual({
+      appendable: [1, 2, 3, 4],
+      a: "a",
+      toggleable: false,
+      b: "b",
+      c: "c"
+    });
+  });
+
+  test("with plain objects with all standalones", () => {
+    const mutation = combineMutations(
+      increment("incrementable"),
+      { a: "a" },
+      toggle("toggleable"),
+      { b: "b", c: "c" }
+    );
+
+    const prevState = {
+      incrementable: 1,
+      a: "b",
+      toggleable: true,
+      b: "c",
+      c: "d"
+    };
+
+    expect(mutation(prevState)).toEqual({
+      incrementable: 2,
+      a: "a",
+      toggleable: false,
+      b: "b",
+      c: "c"
+    });
+  });
+
+  test("with arguments works when called multiple times", () => {
+    const mutation = combineMutations(append("appendable"), { a: "b" })(1);
+
+    const prevState = { appendable: [1], a: "a" };
+    const nextState = mutation(prevState);
+
+    expect(nextState).toEqual({ appendable: [1, 1], a: "b" });
+    expect(mutation(nextState)).toEqual({ appendable: [1, 1, 1], a: "b" });
+  });
 });
