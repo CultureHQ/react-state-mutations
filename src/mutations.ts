38f46dc1413075242e26089b53225fc7311f8f1c
Kevin Deisz <kevin.deisz@gmail.com>
2020-06-01 18:21:33 -0400

    More explicit typings

--- src/mutations.ts
+++ src/mutations.ts
@@ -13,17 +13,19 @@ export const makeStandaloneMutation = <T extends any>(apply: Apply<T>) => (
   (field: Field): StandaloneMutation => {
     const mutation: StandaloneMutation = (state: State) => ({ [field]: apply(state[field]) });
 
-    mutation.standalone = true as true;
+    mutation.standalone = true as const;
     return mutation;
   }
 );
 
 export const makeStandaloneHook = (
-  <T extends any>(apply: Apply<T>, defaultValue: T) => (initialValue = defaultValue) => {
-    const [value, setValue] = useState<T>(initialValue);
-    const onMutate = useCallback(() => setValue(apply), [setValue]);
-    return [value, onMutate];
-  }
+  <T extends any>(apply: Apply<T>, defaultValue: T) => (
+    (initialValue = defaultValue): [T, () => void] => {
+      const [value, setValue] = useState<T>(initialValue);
+      const onMutate = useCallback(() => setValue(apply), [setValue]);
+      return [value, onMutate];
+    }
+  )
 );
 
 export const makeArgumentMutation = <T extends any, A extends any>(apply: ArgumentApply<T, A>) => (
@@ -33,18 +35,16 @@ export const makeArgumentMutation = <T extends any, A extends any>(apply: Argume
       return (state: State) => ({ [field]: curried(state[field]) });
     };
 
-    mutation.standalone = false as false;
+    mutation.standalone = false as const;
     return mutation;
   }
 );
 
 export const makeArgumentHook = (
-  <T extends any, A extends any>(apply: ArgumentApply<T, A>, defaultValue: T) => (
-    (initialValue = defaultValue) => {
+  <T extends any, A extends (arg: any) => any>(apply: ArgumentApply<T, A>, defaultValue: T) => (
+    (initialValue = defaultValue): [T, (argument: A) => void] => {
       const [value, setValue] = useState<T>(initialValue);
-      const onMutate = useCallback(argument => setValue(apply(argument)), [
-        setValue
-      ]);
+      const onMutate = useCallback(argument => setValue(apply(argument)), []);
       return [value, onMutate];
     }
   )
@@ -62,40 +62,37 @@ export const toggleState = (value: boolean): boolean => !value;
 export const toggle = makeStandaloneMutation<boolean>(toggleState);
 export const useToggle = makeStandaloneHook<boolean>(toggleState, true);
 
-export const appendState = <T extends any>(argument: T) => (value: T[]) => (
+export const appendState = <T extends any>(argument: T) => (value: T[]): T[] => (
   value.concat(argument)
 );
 
 export const append = makeArgumentMutation(appendState);
 export const useAppend = makeArgumentHook(appendState, []);
 
-export const concatState = <T extends any>(argument: T | T[]) => (value: T[]) => (
+export const concatState = <T extends any>(argument: T | T[]) => (value: T[]): T[] => (
   value.concat(argument)
 );
 
 export const concat = makeArgumentMutation(concatState);
 export const useConcat = makeArgumentHook(concatState, []);
 
-export const cycleState = <T extends any>(argument: T[]) => (value: T) => (
+export const cycleState = <T extends any>(argument: T[]) => (value: T): T => (
   argument[(argument.indexOf(value) + 1) % argument.length]
 );
 
 export const cycle = makeArgumentMutation(cycleState);
-export const useCycle = <T extends any>(argument: T[]): [T, ReturnType<typeof useCallback>] => {
+export const useCycle = <T extends any>(argument: T[]): [T, () => void] => {
   const [value, setValue] = useState<T>(argument[0]);
 
-  const cycleValue = useCallback(cycleState<T>(argument), [argument]);
-  const onCycle = useCallback(() => setValue(cycleValue), [cycleValue]);
-
-  return [value, onCycle];
+  return [value, (): void => setValue(cycleState<T>(argument))];
 };
 
-export const directState = <T extends any>(argument: T) => () => argument;
+export const directState = <T extends any>(argument: T) => (): T => argument;
 export const direct = makeArgumentMutation(directState);
 
 type FilterArgument<T> = ((value: T) => boolean);
 
-export const filterState = <T extends any>(argument: FilterArgument<T>) => (value: T[]) => (
+export const filterState = <T extends any>(argument: FilterArgument<T>) => (value: T[]): T[] => (
   value.filter(argument)
 );
 
@@ -104,17 +101,19 @@ export const useFilter = makeArgumentHook(filterState, []);
 
 type MapArgument<T> = ((value: T) => T);
 
-export const mapState = <T extends any>(argument: MapArgument<T>) => (value: T[]) => (
+export const mapState = <T extends any>(argument: MapArgument<T>) => (value: T[]): T[] => (
   value.map(argument)
 );
 
 export const map = makeArgumentMutation(mapState);
 export const useMap = makeArgumentHook(mapState, []);
 
-export const mutateState = (argument: object) => (value: object) => ({ ...value, ...argument });
+export const mutateState = (argument: State) => (
+  (value: State): State => ({ ...value, ...argument })
+);
 export const mutate = makeArgumentMutation(mutateState);
 
-export const prependState = <T extends any>(argument: T) => (value: T[]) => (
+export const prependState = <T extends any>(argument: T) => (value: T[]): T[] => (
   [argument].concat(value)
 );
 
@@ -130,7 +129,7 @@ const normalizeMutation = (mutation: (CombineMutation | State)): CombineMutation
 
   const directMutation: StandaloneMutation = () => mutation;
 
-  directMutation.standalone = true as true;
+  directMutation.standalone = true as const;
   return directMutation;
 };
 
@@ -138,7 +137,7 @@ export const combineMutations = (...mutations: (CombineMutation | State)[]): App
   const normalized = mutations.map(normalizeMutation);
 
   if (normalized.some(mutation => !mutation.standalone)) {
-    return (...args: any[]) => (state: State) => {
+    return (...args: any[]) => (state: State): State => {
       let argIndex = -1;
 
       return normalized.reduce((nextState, mutation) => {
@@ -156,7 +155,7 @@ export const combineMutations = (...mutations: (CombineMutation | State)[]): App
     };
   }
 
-  return (state: State) => normalized.reduce(
+  return (state: State): State => normalized.reduce(
     (nextState, mutation) => ({ ...nextState, ...mutation(nextState) }),
     state
   );
